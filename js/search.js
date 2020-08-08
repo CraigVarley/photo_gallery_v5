997f7c6f0a76d4e514c29fd5688652c4bcd3c155
Craig Varley <varley.craig@gmail.com>
2020-08-08 13:28:30 -0400

    final script

--- js/search.js
+++ js/search.js
@@ -1,39 +1,22 @@
-// // start simple: print the contents of data-caption as loop to console - done
-// const anchor = document.querySelectorAll('a');
-// const len = anchor.length;
 
-// for (let i=0;i<len;i++ ){
-//     let cap = document.querySelectorAll('a')[i].getAttribute('data-caption');
-//     console.log(cap);
-// }
-
-// // now accept input string and output live to console - done!
-
-// let input = document.querySelector('input');
-
-// input.addEventListener('input', log); // will always listen, need to loop in function?
-
-// function log(txt) {
-//     console.log(txt.target.value);
-// }
- 
-// now put them together to loop through all data-captions with any input
-const anchor = document.querySelectorAll('a');
-const len = anchor.length; //number of anchor tags in document
-let field = document.getElementById('input'); // search bar
-field.addEventListener('input', search); // listens for search input + calls search()
-const goaway = '<style>display:none;</style>';
+const anchor = document.querySelectorAll('a'); // all the anchors
+const len = anchor.length; // number of anchor tags in document
+let field = document.querySelector('input'); // search bar input
+field.addEventListener('keyup', search, false); // listens for search input + calls search()
 
 function search(txt) {
-    for (let i=0;i<len;i++ ) { // loops through all anchors
-        let cap = document.querySelectorAll('a')[i].getAttribute('data-caption'); //fetches the text in the attribute
-        if (cap.includes(txt.target.value)){
-            document.querySelector('a').innerHTML = '';
-        } else {
-            document.querySelector('a').innerHTML = goaway;
-        }
+    text = txt.target.value; // input of text from the event listener
+    text.toUpperCase();
+    // console.log(`text = ${text}`); // check value of captured tesxt in var stuff
+        for (let i=0;i<len;i++ ) { // loops through all anchors first to last
+            let cap = document.querySelectorAll('a')[i].getAttribute('data-caption'); //fetches the text in the attribute
+            cap.toUpperCase();
+            //console.log(`data-caption equals ${cap}`); // this works fine outputting data-caption string - 3.44pm
+            if (cap.includes(text)){ // if the contents of the data-caption attr include the search text in field
+                document.querySelectorAll('a')[i].style.display = 'grid';; // go to grid, also on delete of field var
+            } else { // if the data caption does NOT contain the search text in field
+                document.querySelectorAll('a')[i].style.display = 'none'; // set display to none
+            }
+       }
     }
-}
-
-//if (p.target.value.includes
 
