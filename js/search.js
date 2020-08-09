5311f607952999d3378ce8f4c93efec67e592458
Craig Varley <varley.craig@gmail.com>
2020-08-09 10:54:57 -0400

    final

--- js/search.js
+++ js/search.js
@@ -6,11 +6,11 @@ field.addEventListener('keyup', search, false); // listens for search input + ca
 
 function search(txt) {
     text = txt.target.value; // input of text from the event listener
-    text.toUpperCase();
+    text = text.toUpperCase();
     // console.log(`text = ${text}`); // check value of captured tesxt in var stuff
         for (let i=0;i<len;i++ ) { // loops through all anchors first to last
             let cap = document.querySelectorAll('a')[i].getAttribute('data-caption'); //fetches the text in the attribute
-            cap.toUpperCase();
+            cap = cap.toUpperCase();
             //console.log(`data-caption equals ${cap}`); // this works fine outputting data-caption string - 3.44pm
             if (cap.includes(text)){ // if the contents of the data-caption attr include the search text in field
                 document.querySelectorAll('a')[i].style.display = 'grid';; // go to grid, also on delete of field var
